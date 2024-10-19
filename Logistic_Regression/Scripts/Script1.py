b62b0d2813f46ba77692e7ae7497c6fb794766ce
Diego2312 <diego.escola2004@gmail.com>
2024-10-20 00:03:42 +0200

    Logistic Regression update

---
+++ Logistic_Regression/Scripts/Script1.py
@@ -0,0 +1,10 @@
+
+#Logistic Regression model
+
+N = list() #Data set. Not real
+m = len(N) #Number of examples in the data set
+
+W = list() #W parameters vector
+b = 0 #Just some value for parameter b
+
+#try later
